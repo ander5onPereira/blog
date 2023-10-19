9ca9f544fc8f3dd7804ac2b9f9b59e07424ded51
Anderson Pereira Rodrigues <anderson.carro.95@gmail.com>
2023-10-18 23:19:40 -0300

    feature: editing page creation

---
+++ src/pages/posts/edit.tsx
@@ -0,0 +1,64 @@
+import usePost from "@/hooks/usePost";
+import React, { useState, ChangeEvent } from "react";
+
+export default function Edit() {
+  const { post } = usePost();
+  const [editedPost, setEditedPost] = useState({
+    title: post?.title || "",
+    content: post?.content || "",
+  });
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setEditedPost({ ...editedPost, [name]: value });
+  };
+  const handleSave = () => {
+    if (editedPost?.title.trim() === "" || editedPost?.content.trim() === "") {
+      alert("Por favor, preencha todos os campos.");
+    } else {
+      // onSave(editedPost);
+      const dateSubmit = {
+        ...editedPost,
+        id:post?.id
+      }
+      console.log({ dateSubmit});
+    }
+  };
+  return (
+    <div className="flex flex-col max-w-xl pt-4 px-5 sm:px-0 mx-auto w-full justify-center items-center h-full">
+      <div className="w-full my-2">
+        <label className="flex" htmlFor="title">
+          Título
+        </label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          placeholder="Digite o título para o post"
+          className=" border-2 rounded  w-full p-2 drop-shadow-md focus:outline-none focus:border-violet-800"
+          value={editedPost.title}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div className="w-full my-2">
+        <label className="flex" htmlFor="content">
+          Conteúdo
+        </label>
+        <textarea
+          name="content"
+          id="content"
+          placeholder="Digite o conteúdo do post"
+          className="border-2 rounded  w-full h-96 p-2 drop-shadow-md focus:outline-none focus:border-violet-800"
+          value={editedPost.content}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="flex justify-end w-full">
+        
+      <button onClick={handleSave} className="bg-violet-800 w-36 py-2 rounded-lg text-white hover:bg-violet-900">Salvar</button>
+    </div>
+    </div>
+  );
+}
