59e5dc923ca74114d899323b89728205ab31d64e
Anderson Pereira Rodrigues <anderson.carro.95@gmail.com>
2023-10-18 23:18:08 -0300

    feature: creation of post detail page

---
+++ src/pages/[id].tsx
@@ -0,0 +1,48 @@
+import usePost from "@/hooks/usePost";
+import { getPost } from "@/services/posts";
+import { GetServerSideProps } from "next";
+import { useEffect } from "react";
+
+interface PostProps {
+  post: {
+    id: string;
+    title: string;
+    content: string;
+  };
+}
+export default function Post({ post }: PostProps) {
+  const { setCurrentPost } = usePost();
+
+  useEffect(() => {
+    setCurrentPost(post);
+  }, [post]);
+
+  return (
+    <main className="container mx-auto mt-4  py-0">
+      <article className=" ">
+        <h1 className="w-full text-center font-semibold text-2xl text-violet-800 border-b-2 pb-2">
+          {post.title}
+        </h1>
+        <p className="mt-5 px-5 sm:px-2">{post.content}</p>
+      </article>
+    </main>
+  );
+}
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.query;
+  const post = await getPost(Number(id));
+  if (post.status !== 200) {
+    return {
+      redirect: {
+        destination: `/${post.status}`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      post: post.data,
+    },
+  };
+};
